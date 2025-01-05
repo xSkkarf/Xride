09b52d78d5684e766e48026bc2ebe994d946b1a9
xSkkarf <mostafa.ayyad.117@gmail.com>
2025-01-05 04:09:55 +0200

    Local time conversion

--- lib/services/reservation_service.dart
+++ lib/services/reservation_service.dart
@@ -4,6 +4,16 @@ import 'package:xride/constants/constants.dart';
 
 class ReservationService {
 
+  dynamic getLocalTime(dynamic response) {
+    if (response['status'] == 'active') {
+      final startTime = DateTime.parse(response['start_time']).toLocal();
+      final endTime = DateTime.parse(response['end_time']).toLocal();
+      response['start_time'] = startTime;
+      response['end_time'] = endTime;
+    }
+    return response;
+  }
+
   Future<dynamic> checkActiveReservation() async {
     final prefs = await SharedPreferences.getInstance();
     final String accessToken = prefs.getString('accessToken')!;
@@ -14,7 +24,10 @@ class ReservationService {
         options: Options(headers: {'Authorization': 'JWT $accessToken'}),
       );
 
-      return response.data;
+      // handle time zone conversion for start time and end time
+      final data = getLocalTime(response.data);
+      
+      return data;
 
     } on DioException catch (e) {
       if (e.response != null) {
@@ -52,9 +65,11 @@ class ReservationService {
         options: Options(headers: {'Authorization': 'JWT $accessToken'}),
         data: requestData,
       );
+
+      final data = getLocalTime(response.data);
       // Handle the response if needed
-      print('Reservation successful: ${response.data}');
-      return response.data;
+      print('Reservation successful: ${data}');
+      return data;
 
     } on DioException catch (e) {
       if (e.response != null) {
